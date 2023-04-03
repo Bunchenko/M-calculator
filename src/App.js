e558e49928dbde58afd6f8e65da498b8801243a4
Andrew Bunchenko <andrew.by.one@gmail.com>
2023-04-03 19:12:18 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import Calculator from "./components/Calculator/Calculator";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import Header from "./components/Header/Header";
+import CalculatorPage from "./pages/CalculatorPage";
+import CalculatorClassPage from "./pages/CalculatorClassPage";
 
 function App() {
 
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <Calculator />
-    </div>
+      <Routes>
+        <Route path="/" element={<CalculatorPage />} />
+        <Route path="functional" element={<CalculatorPage />} />
+        <Route path="class" element={<CalculatorClassPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
